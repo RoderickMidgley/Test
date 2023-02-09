cd7115933e76796614d3e897d64d862c67b8967f
RoderickMidgley <124881590+RoderickMidgley@users.noreply.github.com>
2023-02-09 13:05:58 +0200

    Add files via upload

---
+++ monitor.ps1
@@ -0,0 +1,20 @@
+$ChkSvc=(Get-service -name "w32time" -ErrorAction SilentlyContinue)
+if($ChkSvc.Status -eq "Running")
+{
+echo service already running
+exit 0
+}
+else
+{
+echo "<-Start Result->"
+echo "CSMon_Result= w32time not error not active"
+echo "<-End Result->"
+exit 1
+}
+if("$Error" -ne "$null")
+{
+echo "<-Start Result->"
+echo "CSMon_Result= w32time error not installed"
+echo "<-End Result->"
+exit 1
+}
